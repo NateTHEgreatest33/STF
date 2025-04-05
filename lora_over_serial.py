67e94977f9f5e02d043bc3ec3a6892e2d1146fac
Nate Lenze <nlenze@umich.edu>
2025-04-05 15:16:25 -0400

    Add Lora over serial support script which can be used to help debug mailbox

---
+++ lora_over_serial.py
@@ -0,0 +1,169 @@
+#*********************************************************************
+#
+#   MODULE NAME:
+#       lora_over_serial.py - class for interfacing w/ lora over serial
+#							  in python!
+#
+#   DESCRIPTION:
+#       Provides message API functionality
+#
+#   Copyright 2025 by Nate Lenze
+#*********************************************************************
+
+# note requires python3.11!! (run using python3.11 lora_over_serial.py
+
+#---------------------------------------------------------------------
+#                              IMPORTS
+#---------------------------------------------------------------------
+import serial
+import serial.tools.list_ports
+import time
+
+#---------------------------------------------------------------------
+#                             VARIABLES
+#---------------------------------------------------------------------
+
+#---------------------------------------------------------------------
+#                              CLASSES
+#---------------------------------------------------------------------
+class lora_serial:
+    # ==================================
+    # Constructor
+    # ==================================
+	def __init__(self, port="/dev/tty.usbmodem11101", baud=115200 ):
+		self.ser_conn = serial.Serial(port=port, baudrate=115200, timeout=0)
+		self.ser_conn.close()
+		self.ser_conn.open()
+		self.ser_conn.flush()
+
+    # ==================================
+    # x()
+    # ==================================
+	def __LoraInit(self):
+		#dont need to do anything here since module will already be inited
+		pass
+
+    # ==================================
+    # x()
+    # ==================================
+	def __LoraCheckMessage(self):
+		raise( NotImplemented )
+		pass
+
+    # ==================================
+    # x()
+    # ==================================
+	def __LoraReadMessageSingle(self):
+		raise( NotImplemented )	
+
+    # ==================================
+    # x()
+    # ==================================
+	def __LoraReadMessageMulti(self):
+		return self.LoraReadMessageMulti()
+
+	def LoraReadMessageMulti(self):
+		self.send_cmd( "lora get")
+		response = self.read_and_return()
+
+		if response[:5] == "Error":
+			return []
+
+		#format return
+		rsp_list = response.split()
+		rtn_lst = []
+		for item in rsp_list:
+			rtn_lst.append( int(item) )
+		return rtn_lst
+
+    # ==================================
+    # x()
+    # ==================================
+	def __LoraSetRxMode(self):
+		self.LoraSetRxMode()
+
+	def LoraSetRxMode(self):
+		self.send_cmd( "lora init rx" )
+		self.flush_buffer()
+		pass
+
+    # ==================================
+    # x()
+    # ==================================
+	def __LoraSendMessage( self, messageList, messageSize):
+		return self.LoraSendMessage( messageList, messageSize)
+	
+	def LoraSendMessage( self, messageList, messageSize):
+		command = "lora send"
+		for item in messageList:
+			#NOTE: lora_serial is hardcoded to rx 4 char (0x00), you MUST pad out hex values!
+			command = command + " " + f'{item:#0{4}x}'
+
+		self.send_cmd( command )
+		response = self.read_and_return()
+		pass
+
+    # ==================================
+    # x()
+    # ==================================
+	def read_and_return(self ):
+		time.sleep(.1) # allow buffer to fill in
+		response = self.ser_conn.read(10000).decode('utf-8').split("\r\n")
+		return response[1]
+
+    # ==================================
+    # x()
+    # ==================================
+	def send_cmd(self, cmd ):
+		print( "sending command: {}".format(cmd))
+		cmd = cmd + "\r"
+		self.ser_conn.write( cmd.encode('utf-8') )
+
+    # ==================================
+    # x()
+    # ==================================
+	def flush_buffer(self ):
+		time.sleep(.1)
+		self.ser_conn.read(10000)
+		# self.ser_conn.flush()
+
+
+#---------------------------------------------------------------------
+#                      MAIN FUNCTION
+#---------------------------------------------------------------------
+def main():
+	print("Available Ports: ")
+	ports = list(serial.tools.list_ports.comports())
+	for p in ports:
+		print(p)
+
+	# port_sel = input("please provide port\n")
+	port_sel = "/dev/cu.usbmodem1101"
+
+	l_serial = lora_serial(port=port_sel)
+	
+
+	while( True ):
+		l_serial.flush_buffer()
+		in_cmd = input("Enter Command\n")
+		match in_cmd:
+			case "rx":
+				rtn_data = l_serial.LoraReadMessageMulti()
+				print( "rtn data: {}".format( [hex(i) for i in rtn_data] ) )
+			case "tx":
+				l_serial.LoraSendMessage( [0x01, 0x02, 0x03], 3 )
+				l_serial.LoraSetRxMode()
+			case "init rx":
+				l_serial.__LoraSetRxMode()
+			case _:
+				print("No Command matches")
+	
+	return
+
+
+
+#---------------------------------------------------------------------
+#                      MAIN TEST LOOP SWRS0001
+#---------------------------------------------------------------------
+if __name__ == "__main__":
+  main()
